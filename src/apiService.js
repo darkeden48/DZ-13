91f6f99feb9f4cdcdbe192b2a19b4b086486be1f
darkeden48 <dzalex72@list.ru>
2022-11-27 15:57:42 +0200

    save

--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,7 @@
 // https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=24335530-1fa5676597020c031a07a1cad
 const KEY = '24335530-1fa5676597020c031a07a1cad';
 let PAGE = 1;
-let VALUE = 'fire';
+let VALUE = '';
 
 function apiService(){
  return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${VALUE}&page=${PAGE}&per_page=12&key=${KEY}`)
@@ -9,8 +9,11 @@ function apiService(){
  .then(data=>{return data.hits}); 
 }
 
-const pager = () => {
-    PAGE +=1;
+const pager = (page) => {
+    PAGE+=1;
+    if(page===1){
+        PAGE=1 
+    }
 }
 
 const valuer = (searchValue) => {
--- src/index.js
+++ src/index.js
@@ -3,22 +3,48 @@ import card from "./templates/card.hbs"
 
 const loadMore = document.querySelector('.button-more');
 const list = document.querySelector('.gallery');
-const form = document.querySelector('.search-form');
+const form = document.querySelector('#search-form');
 const input = document.querySelector('.input');
+const btnUp = document.querySelector('.btn-up');
+
+const toUp = () => {
+    form.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+}
 
 const nextPage = () => {
     pager();
+    apiService().then(appendMarkup);
 }
 
 const search = (e) => {
     e.preventDefault();
+    form.classList.remove('search-form')
+    if(input.value===''){list.innerHTML = 'Enter picture name'; return}
+    list.innerHTML = '';
     valuer(input.value);
-    console.log(apiService());
-    const op = apiService().then(data=>data.map(el=>{console.log(el);return list.insertAdjacentHTML('beforeend', card(el))}));
+    pager(1);
+    apiService().then(appendMarkup);
+    loadMore.disabled=false;
+    btnUp.disabled=false;
+    input.value='';
+}
+
+const appendMarkup = (img)=> {
+    console.log(img)
+    if(img.length===0){list.innerHTML = 'Picture is`n finded'; return}
+     img.map(el=>{list.insertAdjacentHTML('beforeend',card(el))});
+     loadMore.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
 }
 
 form.addEventListener('submit', search);
 loadMore.addEventListener('click', nextPage);
+btnUp.addEventListener('click', toUp)
 
 
 
@@ -30,29 +56,3 @@ loadMore.addEventListener('click', nextPage);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const element = document.getElementById('.my-element-selector');
-// element.scrollIntoView({
-//   behavior: 'smooth',
-//   block: 'end',
-// });
